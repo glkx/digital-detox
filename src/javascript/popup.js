a675c8cbeee3e79d39c67caa739121002c00250d
Guido Schalkx <guido@kyver.co>
2018-12-29 11:55:53 +0100

    Replace deprecated extension getURL

--- src/javascript/popup.js
+++ src/javascript/popup.js
@@ -129,7 +129,7 @@ function removeWebsite() {
 
 function openOptions() {
 	browser.tabs.create({
-		url: browser.extension.getURL('/options.html')
+		url: browser.runtime.getURL('/options.html')
 	});
 	window.close();
 }
