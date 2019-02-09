1634500aa64d33685d0d9002bff3b24a10e795fa
Guido Schalkx <guido@kyver.co>
2019-02-09 11:39:53 +0100

    Add comments

--- src/javascript/modules/domain.js
+++ src/javascript/modules/domain.js
@@ -6,15 +6,20 @@ export default class Domain {
 	static parseURL(url) {
 		const parseURL = new URL(url);
 
+		// Check url is http or https address
 		if (['http:', 'https:'].indexOf(parseURL.protocol) === -1) {
 			return false;
 		}
 
+		// Return domain
 		return parseURL.hostname.replace(/^www\./, '');
 	}
 
 	static async getCurrent() {
+		// Get current active tab
 		const currentTab = await Tabs.getCurrent();
+
+		// Parse tab url and return
 		return this.parseURL(currentTab.url);
 	}
 }
